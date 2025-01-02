f149ec054dce3c3bb5be3a9b14ee876f17be4c8c
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-02 17:34:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <gtk/gtk.h>
 #include <zlib.h>
+#include <sys/stat.h>
 
 #define CHUNK 16384
 
 // Function prototypes
-void compressFile(const char *inputFile, const char *outputFile);
+void compressFileWithLevel(const char *inputFile, const char *outputFile, int level);
 void decompressFile(const char *inputFile, const char *outputFile);
+long getFileSize(const char *filename);
+void on_compress_button_clicked(GtkWidget *widget, gpointer data);
+void on_decompress_button_clicked(GtkWidget *widget, gpointer data);
+void on_file_chosen(GtkWidget *widget, gpointer data);
+void update_progress_bar(GtkProgressBar *progressBar, double fraction);
+
+GtkWidget *progressBar;
+GtkWidget *statusLabel;
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Usage: %s [compress|decompress] <input_file> <output_file>\n", argv[0]);
-        return 1;
-    }
+    gtk_init(&argc, &argv);
 
-    if (strcmp(argv[1], "compress") == 0) {
-        compressFile(argv[2], argv[3]);
-    } else if (strcmp(argv[1], "decompress") == 0) {
-        decompressFile(argv[2], argv[3]);
-    } else {
-        printf("Invalid operation: %s. Use 'compress' or 'decompress'.\n", argv[1]);
-        return 1;
-    }
+    // Create the window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "File Compressor/Decompressor");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+
+    // Create the box container
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    // Create and add buttons
+    GtkWidget *compressButton = gtk_button_new_with_label("Compress File");
+    GtkWidget *decompressButton = gtk_button_new_with_label("Decompress File");
+    gtk_box_pack_start(GTK_BOX(vbox), compressButton, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), decompressButton, TRUE, TRUE, 0);
+
+    // Create progress bar
+    progressBar = gtk_progress_bar_new();
+    gtk_box_pack_start(GTK_BOX(vbox), progressBar, FALSE, FALSE, 0);
+
+    // Create status label
+    statusLabel = gtk_label_new("Status: Ready");
+    gtk_box_pack_start(GTK_BOX(vbox), statusLabel, FALSE, FALSE, 0);
+
+    // Connect signals
+    g_signal_connect(compressButton, "clicked", G_CALLBACK(on_compress_button_clicked), NULL);
+    g_signal_connect(decompressButton, "clicked", G_CALLBACK(on_decompress_button_clicked), NULL);
+
+    // Show all components
+    gtk_widget_show_all(window);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_main();
 
     return 0;
 }
 
-// Compress a file using zlib
-void compressFile(const char *inputFile, const char *outputFile) {
+void on_compress_button_clicked(GtkWidget *widget, gpointer data) {
+    GtkWidget *dialog = gtk_file_chooser_dialog_new("Select File to Compress", NULL,
+                                                    GTK_FILE_CHOOSER_ACTION_OPEN,
+                                                    "Cancel", GTK_RESPONSE_CANCEL,
+                                                    "Open", GTK_RESPONSE_ACCEPT,
+                                                    NULL);
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+        char *inputFile = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+        char *outputFile = "compressed_file.gz";  // Hardcoded for demo purposes
+        int level = Z_BEST_COMPRESSION;          // Default compression level
+
+        compressFileWithLevel(inputFile, outputFile, level);
+        g_free(inputFile);
+    }
+    gtk_widget_destroy(dialog);
+}
+
+void on_decompress_button_clicked(GtkWidget *widget, gpointer data) {
+    GtkWidget *dialog = gtk_file_chooser_dialog_new("Select File to Decompress", NULL,
+                                                    GTK_FILE_CHOOSER_ACTION_OPEN,
+                                                    "Cancel", GTK_RESPONSE_CANCEL,
+                                                    "Open", GTK_RESPONSE_ACCEPT,
+                                                    NULL);
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+        char *inputFile = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+        char *outputFile = "decompressed_file.txt";  // Hardcoded for demo purposes
+
+        decompressFile(inputFile, outputFile);
+        g_free(inputFile);
+    }
+    gtk_widget_destroy(dialog);
+}
+
+void compressFileWithLevel(const char *inputFile, const char *outputFile, int level) {
     FILE *in = fopen(inputFile, "rb");
     if (in == NULL) {
         perror("Error opening input file");
@@ -43,7 +106,7 @@ void compressFile(const char *inputFile, const char *outputFile) {
     }
 
     z_stream strm = {0};
-    if (deflateInit(&strm, Z_BEST_COMPRESSION) != Z_OK) {
+    if (deflateInit(&strm, level) != Z_OK) {
         fprintf(stderr, "Error initializing zlib compression.\n");
         fclose(in);
         fclose(out);
@@ -52,10 +115,16 @@ void compressFile(const char *inputFile, const char *outputFile) {
 
     unsigned char inBuf[CHUNK];
     unsigned char outBuf[CHUNK];
-    int flush;
+    long totalFileSize = getFileSize(inputFile);
+    long processedSize = 0;
 
     do {
         strm.avail_in = fread(inBuf, 1, CHUNK, in);
+        processedSize += strm.avail_in;
+
+        // Update the progress bar
+        update_progress_bar(GTK_PROGRESS_BAR(progressBar), (double)processedSize / totalFileSize);
+
         if (ferror(in)) {
             fprintf(stderr, "Error reading input file.\n");
             deflateEnd(&strm);
@@ -63,7 +132,8 @@ void compressFile(const char *inputFile, const char *outputFile) {
             fclose(out);
             return;
         }
-        flush = feof(in) ? Z_FINISH : Z_NO_FLUSH;
+
+        int flush = feof(in) ? Z_FINISH : Z_NO_FLUSH;
         strm.next_in = inBuf;
 
         do {
@@ -73,15 +143,15 @@ void compressFile(const char *inputFile, const char *outputFile) {
             fwrite(outBuf, 1, CHUNK - strm.avail_out, out);
         } while (strm.avail_out == 0);
 
-    } while (flush != Z_FINISH);
+    } while (!feof(in));
 
     deflateEnd(&strm);
     fclose(in);
     fclose(out);
-    printf("File compressed successfully.\n");
+
+    gtk_label_set_text(GTK_LABEL(statusLabel), "Compression complete!");
 }
 
-// Decompress a file using zlib
 void decompressFile(const char *inputFile, const char *outputFile) {
     FILE *in = fopen(inputFile, "rb");
     if (in == NULL) {
@@ -106,9 +176,16 @@ void decompressFile(const char *inputFile, const char *outputFile) {
 
     unsigned char inBuf[CHUNK];
     unsigned char outBuf[CHUNK];
+    long totalFileSize = getFileSize(inputFile);
+    long processedSize = 0;
 
     do {
         strm.avail_in = fread(inBuf, 1, CHUNK, in);
+        processedSize += strm.avail_in;
+
+        // Update the progress bar
+        update_progress_bar(GTK_PROGRESS_BAR(progressBar), (double)processedSize / totalFileSize);
+
         if (ferror(in)) {
             fprintf(stderr, "Error reading input file.\n");
             inflateEnd(&strm);
@@ -116,8 +193,10 @@ void decompressFile(const char *inputFile, const char *outputFile) {
             fclose(out);
             return;
         }
+
         if (strm.avail_in == 0)
             break;
+
         strm.next_in = inBuf;
 
         do {
@@ -132,5 +211,20 @@ void decompressFile(const char *inputFile, const char *outputFile) {
     inflateEnd(&strm);
     fclose(in);
     fclose(out);
-    printf("File decompressed successfully.\n");
+
+    gtk_label_set_text(GTK_LABEL(statusLabel), "Decompression complete!");
+}
+
+long getFileSize(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) == 0) {
+        return st.st_size;
+    } else {
+        perror("Error retrieving file size");
+        return -1;
+    }
+}
+
+void update_progress_bar(GtkProgressBar *progressBar, double fraction) {
+    gtk_progress_bar_set_fraction(progressBar, fraction);
 }
